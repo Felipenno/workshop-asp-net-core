53d73074c910e362dc82d37fb26827aa36afcbf5
Felipe Nunes <felipe.ht3@gmail.com>
2019-08-14 14:43:21 -0300

    Other entities and second migration

---
+++ Models/Enums/SalesStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
---
+++ Models/SalesRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+
+
+}
